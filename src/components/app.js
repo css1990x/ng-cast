874ca6461db9c2285e27a28aaaf095c9aa63ddb1
css1990x <css1990x>
2017-10-27 16:22:26 -0700

    Puts the first video in the player

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ angular.module('video-player')
 .component('app', {
   controller: function() {
     this.videos = exampleVideoData;
-    this.playing = exampleVideoData[0];
+    this.currentVideo = exampleVideoData[0];
   },
   templateUrl: 'src/templates/app.html',
   bindings: {
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -3,6 +3,11 @@ angular.module('video-player')
 .component('videoPlayer', {
   templateUrl: 'src/templates/videoPlayer.html',
   bindings: {
-
+    cv: '<' 
   }
+})
+.filter('youtubeEmbedUrl', function($sce) {
+  return function(videoId) {
+    return $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + videoId);
+  };
 });
